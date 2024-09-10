7a8ca860a45cb0df48660cdc5beb320cc3dd759b
smh10 <smh10.mh@gmail.com>
2024-09-09 22:05:17 -0400

    First commit: Structuring the project!

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
+}
